f2b70926e6ca313cce1a47ee76d485925d3a966c
Mohamed Sultan <Mohamed.Sultan@simulation-systems.co.uk>
2016-10-20 12:25:06 +0100

    Functions.

---
+++ Concepts/lamdaExpressions/src/main/java/com/example/lambda/functions/Main.java
@@ -0,0 +1,14 @@
+package com.example.lambda.functions;
+
+/**
+ * @author MikeW
+ */
+public class Main {
+
+  public static void main(String[] args) {
+  
+    NameTestOld.main(args);
+    NameTestNew.main(args);
+
+  }
+}
